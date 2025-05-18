fa2bfa3efdfcc0e681a1252156d7ed0282512f29
Lightmaker777 <j.kochkorian@gmx.de>
2025-05-18 17:14:08 +0200

    .env

--- run.py
+++ run.py
@@ -5,8 +5,12 @@ from app.models.project import Project
 from app.models.file import File
 from app.models.timesheet import Timesheet
 from app.models.vacation import Vacation
+from dotenv import load_dotenv
+import os
 
 app = create_app()
+
+load_dotenv()
 
 @app.before_first_request
 def create_tables():
@@ -14,10 +18,13 @@ def create_tables():
     
     # Erstelle Admin-Benutzer, falls keine Benutzer vorhanden sind
     if User.query.count() == 0:
-        admin = User(username='admin', role='admin')
-        admin.set_password('admin1234')  # In der Produktion ein sicheres Passwort verwenden
-        db.session.add(admin)
-        db.session.commit()
+        # Prüfe, ob Umgebungsvariablen existieren
+        if os.getenv('ADMIN_USERNAME') and os.getenv('ADMIN_PASSWORD'):
+            admin = User(username=os.getenv('ADMIN_USERNAME'), 
+                        role='admin')
+            admin.set_password(os.getenv('ADMIN_PASSWORD'))
+            db.session.add(admin)
+            db.session.commit()
 
 if __name__ == '__main__':
     app.run(debug=True)
